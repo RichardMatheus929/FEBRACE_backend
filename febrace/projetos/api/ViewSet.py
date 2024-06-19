4bdf5f07663585a52d7995a32c8aea71c75acc9f
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-19 14:39:40 -0300

    Sobreescrevendo o método list de todos os projetos

--- febrace/projetos/api/ViewSet.py
+++ febrace/projetos/api/ViewSet.py
@@ -5,12 +5,29 @@ from rest_framework import status
 from projetos.models import Projeto
 from .Serializers import ProjetoSerializer
 
+import random
 
 class ProjetoViewSet(ModelViewSet):
     queryset = Projeto.objects.all()
     serializer_class = ProjetoSerializer
     # filter_backends = [filters.SearchFilter]
     filterset_fields = ['id', 'ano']
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        allprojetos = serializer.data
+
+        random.shuffle(allprojetos)
+
+        return Response(allprojetos)
 
     # def list(self, request, *args, **kwargs):
     #     # Obtém os parâmetros da query string
