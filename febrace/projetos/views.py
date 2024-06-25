1e4f997ef8f1c74cd0057b7102b40d0a24b55123
Richard Matheus <richard.ataliba.711@ufrn.edu.br>
2024-06-25 16:54:41 -0300

    Adicionando os anos no retorno do endpoint

--- febrace/projetos/views.py
+++ febrace/projetos/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
 
-from rest_framework.views import Response,APIView
+from rest_framework.views import Response, APIView
 
 from .models import Projeto
 
 # Create your views here.
 
+
 class InfoProject(APIView):
-    def get(self,request,id_project):
+
+    def get(self, request, id_project):
 
         escola = Projeto.objects.filter(id=id_project).first().escola
         name = Projeto.objects.filter(id=id_project).first().nome
 
         quantidade_projetos = Projeto.objects.filter(nome__icontains=name).count()
+
         categorias_premiacoes = Projeto.objects.filter(nome__icontains=name).values_list('categoria_premiacao', flat=True)
         categorias_premiacoes = list(categorias_premiacoes)
 
-        info = {
-            'escola':escola,
-            'premios':quantidade_projetos,
-            'categorias':categorias_premiacoes
-            }
+        anos = Projeto.objects.filter(nome__icontains=name).values_list('ano', flat=True)
+        anos = set(anos)
 
-        return Response ({'info':str(info)})
+        info = {
+            'escola': escola,
+            'premios': quantidade_projetos,
+            'categorias': " / ".join(categorias_premiacoes),
+            'ano': " / ".join(anos)
+        }
+
+        return Response(info)
